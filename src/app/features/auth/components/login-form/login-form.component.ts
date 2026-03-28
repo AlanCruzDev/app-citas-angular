6e329059ff872ae9718503e52e4ce554aa126299
AlanCruzDev <alancruzv50@gmail.com>
2026-03-28 01:38:58 -0600

    bug corriedo de 10 dijitos

--- src/app/features/auth/components/login-form/login-form.component.ts
+++ src/app/features/auth/components/login-form/login-form.component.ts
@@ -1,54 +1,37 @@
-
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { LoginRequest } from '../../models/LoginRequest'; //Importe el login.request
-
-
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.css'],
-
 })
 export class LoginFormComponent {
-
   //Se mandan a llamar las variables desde el LoginRequest, ya no necesito especificar que tipo variables son, ya lo haces desde el request.
-  @Output() loginSubmit = new EventEmitter<LoginRequest>(); 
-  
-  loginForm: FormGroup;
+  @Output() loginSubmit = new EventEmitter<LoginRequest>();
 
+  public loginForm: FormGroup;
 
   constructor(private fb: FormBuilder) {
     this.loginForm = this.fb.group({
-      numPhone: ['', [Validators.required,
-      Validators.pattern("^[0-9]*$"), //Validar que solo se acepten números
-      Validators.maxLength(10),
-      Validators.minLength(10)
-      ]],
-      //Valida que sean minimo 8 caracteres la contraseña
-      password: ['', [Validators.required,
-      Validators.minLength(8)
-      ]]
-
-
-    })
+      numPhone: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
   }
 
-
+  public soloNumeros(event: any) {
+    event.target.value = event.target.value.replace(/[^0-9]/g, '');
+    event.target.value = event.target.value.slice(0, 10);
+  }
 
   //metodo para que el cuando presiones el boton mande la informacion al  padre
-  onSubmit(): void {
+  public onSubmit(): void {
     if (this.loginForm.invalid) {
       return;
     }
     //Manda los datos al componente padre cuando ocurre el evento de dar click en btn iniciar sesión
     this.loginSubmit.emit(this.loginForm.value);
-
-    console.log('Usuario: ', this.loginForm.value);
     this.loginForm.reset();
   }
 }
-
-
-
--- src/app/features/auth/pages/login-pages/login-page.component.ts
+++ src/app/features/auth/pages/login-pages/login-page.component.ts
@@ -1,23 +1,21 @@
 import { Component } from '@angular/core';
-import { LoginRequest } from '../../models/LoginRequest'; //se importo el loginrequest 
-
+import { LoginRequest } from '../../models/LoginRequest'; //se importo el loginrequest
 
 @Component({
   selector: 'app-login',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css']
+  styleUrls: ['./login-page.component.css'],
 })
 export class LoginPageComponent {
+  numPhone: string = '';
+  password: string = '';
 
-  numPhone: string = "";
-  password: string = "";
+  //se cambio el parametro y ya recibe el loginrequest
+  setInfoLogin(e: LoginRequest) {
+    this.numPhone = e.numPhone;
+    this.password = e.password;
 
-  //se cambio el parametro y ya recibe el loginrequest 
-  setInfoLogin(e: LoginRequest ){
-     this.numPhone = e.numPhone ;
-     this.password = e.password;
-
-     console.log('se recibio el numero:', this.numPhone);
+    console.log('se recibio el numero:', this.numPhone);
     console.log('se recibio la contraseña:', this.password);
   }
 }
